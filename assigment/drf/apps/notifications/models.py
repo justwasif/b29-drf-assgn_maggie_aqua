f00b96e0ed7a603e2bca092735df78ab2a10d868
wasif <wasif.iit.ansari@gmail.com>
2026-05-23 22:29:04 +0530

    refactor: updated project models

--- assigment/drf/apps/notifications/models.py
+++ assigment/drf/apps/notifications/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from apps.common.models import TimeStampe
+from apps.common.constants import Notification
+
+from apps.users.models import User
+import apps.projects.models import (Project)
