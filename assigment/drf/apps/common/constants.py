c051fe57ef60612ba18f29c1f34a3fe26774981b
wasif <wasif.iit.ansari@gmail.com>
2026-05-23 14:44:16 +0530

    feat:created studio models and a comment model

---
+++ assigment/drf/apps/common/constants.py
@@ -0,0 +1,22 @@
+ROLES={
+    {'ADMIN',"Admin"},
+    {'PROJECT_LEAD','project lead'},
+    {'WRITER',"writer"},
+    {"REVIEWER",'REVIEWRE'},
+    {'CLIENT_VIEWER','client_viewer'},
+    {"DESIGNER",'designer'}
+
+}
+
+ApprovedStages={
+    {'PENDING',"pending"},
+    {"APPROVED",'approved'},
+    {},
+
+}
+Notification={
+    {"Comment",'comment'},
+    {"Task",'task'},
+    {"Stage","stage_moved"},
+    {'Deadline','deadline'},
+}
--- assigment/drf/apps/studios/models.py
+++ assigment/drf/apps/studios/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+from apps.common.models import TimeStampe
+from apps.common.constants import ROLES
+from apps.users.models import User
+
+class Studio(TimeStampe):
+    name=models.CharField(max_length=50)
+    description=models.TextField()
+    created_by=models.ForeignKey(User,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
+
+
+class StudioMembership(TimeStampe):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    studio=models.ForeignKey(Studio,on_delete=models.CASCADE)
+    role=models.CharField(max_length=50, choices=ROLES)
+
+    def __str__(self):
+        return f"{self.user.email} - {self.studio.name}"
+
+
