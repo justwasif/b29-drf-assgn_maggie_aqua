7e3d5630d608aa8cd9145855de29106d5e7e3b59
wasif <wasif.iit.ansari@gmail.com>
2026-05-23 23:40:14 +0530

    feat: add Task_Attachement model

--- assigment/drf/apps/attachments/models.py
+++ assigment/drf/apps/attachments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from apps.common.models import TimeStampe
+from apps.users.models import User
+from apps.projects.models import Task
 
 # Create your models here.
+class Task_Attachement(TimeStampe):
+    task=models.ForeignKey(Task,on_delete=models.CASCADE)
+    uploaded_by=models.ForeignKey(User,on_delete=models.CASCADE)
+    description=models.TextField()
+    file_url=models.CharField(max_length=2000)
+
