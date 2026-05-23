d3b0c7d526adb9d1fab9a3d2ac0e060cf3b4608e
wasif <wasif.iit.ansari@gmail.com>
2026-05-23 18:21:16 +0530

    feat: add Discussion_Thread and Comment models

--- assigment/drf/apps/discussions/models.py
+++ assigment/drf/apps/discussions/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
+from apps.common.models import TimeStampe
+from apps.users.models import User
+from apps.projects.models import (Task,Stage)
+
 # Create your models here.
+class Discussion_Thread(TimeStampe):
+    stage = models.ForeignKey(
+        Stage,
+        on_delete=models.CASCADE,
+    )
+
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    def __str__(self):
+        return f"Thread {self.id}"
+    
+
+class Comment(TimeStampe):
+    thread = models.ForeignKey(
+        Discussion_Thread,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+       
+    )
+
+    message = models.TextField()
+
+
+    def __str__(self):
+        return f"Comment by {self.user.email}"
