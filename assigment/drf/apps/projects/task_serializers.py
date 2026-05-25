d821f5aa976f43c60aa0e0df16453e190f628e9f
falcon <falgunidhingra9@gmail.com>
2026-05-26 03:40:55 +0530

    feat: added isolation

--- assigment/drf/apps/projects/task_serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Task
-
-class TaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Task
-        fields = ['id', 'project', 'title', 'description', 'priority', 'deadline', 'stage', 'assigned_to', 'created_by', 'created_at', 'updated_at']
-        read_only_fields = ['created_by']
--- assigment/drf/apps/projects/urls.py
+++ assigment/drf/apps/projects/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import ProjectViewSet, TaskViewSet
+from .views import ProjectViewSet, TaskViewSet, StageViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('projects', ProjectViewSet, basename='projects')
 router.register('tasks', TaskViewSet, basename='tasks')
+router.register('stages', StageViewSet, basename='stages')
 
 urlpatterns = [
     path('', include(router.urls)),
