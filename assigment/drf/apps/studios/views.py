7671814bae4cdb7959dbd1199cea711887380d06
wasif <wasif.iit.ansari@gmail.com>
2026-05-26 14:20:42 +0530

    feat: implement Studio and StudioMembership viewsets with permissions

--- assigment/drf/apps/studios/views.py
+++ assigment/drf/apps/studios/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from apps.studios.models import (Studio,StudioMembership)
+from apps.studios.serializers import (StudioSerializer,StudioMembershipSerializer) 
+from apps.users.permissions import (IsAdmin,IsProjectLeadorAdmin,IsClientReadOnly,IsReviewer,IsTeamMember)
+
+class StudioViewSet(viewsets.ModelViweSet):
+    queryset=Studio.objects.all()
+    serializer_class=StudioSerializer
+    Permission_classes=[IsAdmin,IsProjectLeadorAdmin]
+
+    def perform_create(self,serializer):
+    serializer.save(uploded_by=self.request.user)
+
+class StudioMembershipViewSet(viewsets.ModelViewSet):
+    queryset=StudioMembership.objects.all()
+    serializer_class=StudioMembershipSerializer
+    permission_classes=[IsClientReadOnly,IsTeamMember,IsReviewer]
+
+
+    
+
+
+
 
 # Create your views here.
