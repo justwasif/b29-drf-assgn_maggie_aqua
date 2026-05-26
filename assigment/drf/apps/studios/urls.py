c6785cdc8e60b8c98c30221bd7c0c23acdfa5e67
wasif <wasif.iit.ansari@gmail.com>
2026-05-26 14:27:22 +0530

    feat: add Studio and StudioMembership URL routing

---
+++ assigment/drf/apps/studios/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from apps.studios.views import (StudioViewSet,StudioMembershipViewSet)
+
+router=DefaultRouter()
+router.register('studio',StudioViewSet,basename='studio')
+router.register('studiomember',StudioMembershipViewSet,basename='studiomember')
+
+urlpattens=[
+    path('',include(router.urls)),
+]
